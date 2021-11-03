20d9c34f33a34c8e212b65d647e68ca3227124a0
Chinedu Okechukwu <okechukwu.nedu@gmail.com>
2021-11-03 16:16:45 -0700

    commit naiveBayes

---
+++ analysis/03_naiveBayes.R
@@ -0,0 +1,65 @@
+# Split Data into Training and Testing set --------------------------------
+
+# set random seed to 154
+set.seed(154)
+
+# Randomly create 75% index rows to use for split 
+sampleSet <- sample(nrow(mktgCmpgn),
+                    round(nrow(mktgCmpgn) * 0.75),
+                    replace = FALSE)
+
+# Put the records from 75%  sample into mktgCmpgnTraining
+mktgCmpgnTraining <- mktgCmpgn[sampleSet,]
+
+
+# Put the records from 75%  sample into mktgCmpgnTesting
+mktgCmpgnTesting <- mktgCmpgn[-sampleSet,]
+
+
+# Build the model ---------------------------------------------------------
+
+# Generate the Naive Bayes model to predict mktgCmpgn
+
+mktgCmpgnModel <- naiveBayes(formula = Response ~ .,
+                                data = mktgCmpgnTraining,
+                                laplace = 1)
+
+# Build probability for each record in testing dataset
+
+mktgCmpgnProbability <- predict(mktgCmpgnModel,
+                                   mktgCmpgnTesting,
+                                   type = "raw")
+
+# Display probability 
+print(mktgCmpgnProbability)
+
+
+# Predict classes for each record in testing dataset
+mktgCmpgnPrediction<- predict(mktgCmpgnModel,
+                                 mktgCmpgnTesting,
+                                 type = "class")
+
+
+# Display prection in console
+print(mktgCmpgnPrediction)
+
+
+# Evaluating Performance --------------------------------------------------
+
+# Evaluate model with confusion matrix
+
+mktgCmpgnConfusionMatrix <- table(mktgCmpgnTesting$Response,
+                                     mktgCmpgnPrediction)
+
+# Display confusion matrix
+
+print(mktgCmpgnConfusionMatrix)
+
+
+# Determine the predictive accuracy
+predictiveAccuracy <- sum(diag(mktgCmpgnConfusionMatrix)) / 
+  nrow(mktgCmpgnTesting)
+
+# Display the predictive accuracy in the console
+print(predictiveAccuracy)
+
